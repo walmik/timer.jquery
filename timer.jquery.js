2ff76f0ccf27ec1b8fb4d5999de1e14ad909f4c6
Walmik <walmik.deshpande@gmail.com>
2012-02-20 14:06:29 +0530

    streamlined the plugin declaration and added the pauseTimer function

--- timer.jquery.js
+++ timer.jquery.js
@@ -1,7 +1,12 @@
-/*
- timer.jquery.js
- ---------------
- display a timer inside a div or span or any valid html tag
+/*!
+ * jQuery timer plugin: display a timer inside a div or span or any valid html tag
+ * Examples and documentation at: http://walmik.info/demos/timer.jquery
+ * version 1 (20-FEB-2012)
+ * Requires jQuery v1.3.2 or later
+ * Dual licensed under the MIT and GPL licenses:
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ * Author: Walmik Deshpande
  
  usage:
  $("#timerDiv").initTimer();
@@ -12,14 +17,8 @@
  $("#timerDiv").startTimer();   //not implemented yet
   
 */
-
-jQuery.fn.timer = function(options)
+;(function($)
 {
-    
-    var element = $(this);
-    var params = jQuery.extend({showHours: false}, options);
-
-    
     var secs = 0;
     var mins = 0;
     var hrs = 0;
@@ -31,51 +30,74 @@ jQuery.fn.timer = function(options)
     var delay = 1000;
     var type = "string";
     
-    initTimer();
     
-    function initTimer()
+    $.fn.timer = function(method)
     {
-        console.log("start");
-        timerId = setInterval(incrementTime, delay);
-        isTimerRunning = true;
-        $(element).attr("isTimerRunning", true);            //check this attr later for pausing or restarting
-    }
-    
-    function showTime()
-    {
-        console.log("show time");
-        if(params.showHours) $(element).html(hrsStr + ":" + minsStr + ":" + secsStr);
-        else $(element).html(minsStr + ":" + secsStr);
-    }
-    
-    function incrementTime()
-    {
-        console.log("increment time");
-        if(secs < 10) secsStr = "0" + secs;
-        else secsStr = secs;
-        
-        if(mins < 10) minsStr = "0" + mins;
-        else minsStr = mins;
-        
-        if(hrs < 10) hrsStr = "0" + hrs;
-        else hrsStr = hrs;
-        
-        showTime();
-        
-        secs++;
-        
-        if(secs % 60 == 0)
+        var element = this;
+        var settings = $.extend({showHours: false});
+
+        switch(method)
         {
-            mins++;
-            secs = 0;
+            case "init":
+                showTimer();
+                secs++;     //to avoid the 1 second gap that gets created if the seconds are not incremented
+                initTimer();
+                break;
+            
+            case "pause":
+                pauseTimer();
+                break;
+        }
+
+        function pauseTimer()
+        {
+            clearInterval(timerId);
         }
         
-        //handle time exceeding 60 mins!
-        if(mins > 59 && mins % 60 == 0)
+        function initTimer()
         {
-            hrs++;
-            mins = 0;
+            console.log("start");
+            timerId = setInterval(incrementTime, delay);
+            isTimerRunning = true;
+            $(element).attr("isTimerRunning", true);            //check this attr later for pausing or restarting
         }
+        
+        function showTimer()
+        {
+            //console.log("show time");
+            if(settings.showHours) $(element).html(hrsStr + ":" + minsStr + ":" + secsStr);
+            else $(element).html(minsStr + ":" + secsStr);
+        }
+        
+        function incrementTime()
+        {
+            //console.log("increment time");
+            if(secs < 10) secsStr = "0" + secs;
+            else secsStr = secs;
+            
+            if(mins < 10) minsStr = "0" + mins;
+            else minsStr = mins;
+            
+            if(hrs < 10) hrsStr = "0" + hrs;
+            else hrsStr = hrs;
+            
+            showTimer();
+            
+            secs++;
+            
+            if(secs % 60 == 0)
+            {
+                mins++;
+                secs = 0;
+            }
+            
+            //handle time exceeding 60 mins!
+            if(mins > 59 && mins % 60 == 0)
+            {
+                hrs++;
+                mins = 0;
+            }
+        }
+        
     }
-    
-}
+})(jQuery);
