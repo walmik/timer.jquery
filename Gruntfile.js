250516e3c7f75bdd7bb88b4745b3da8f790a97e0
Walmik Deshpande <walmik.deshpande@gmail.com>
2013-05-08 19:54:18 -0700

    added Gruntfile with uglify and watch

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function (grunt) {
+    grunt.initConfig({    
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+          options: {
+            banner: '/*! <%= pkg.name %> <%= pkg.version %> <%=grunt.template.today("yyyy-mm-dd")%>*/'
+          },
+          dist: {
+            src: ['src/timer.jquery.js'],
+            dest: 'dist/timer.query.min.js'
+          },
+        },
+        watch: {
+          scripts: {
+            files: ['src/timer.jquery.js'],
+            tasks: ['uglify'],
+            options: {
+              nospawn: true
+            }
+          }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    //register default task
+    grunt.registerTask('default', 'watch');
+}
