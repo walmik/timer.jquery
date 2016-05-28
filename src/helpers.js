13ed4d3e14f3ba588313f653c1c39528446bf852
Walmik <walmik.deshpande@gmail.com>
2016-05-27 21:36:23 -0700

    Finalized timeFormatToSeconds

--- src/helpers.js
+++ src/helpers.js
@@ -1,35 +1,85 @@
+const THIRTYSIXHUNDRED = 3600;
+const SIXTY = 60;
+const TEN = 10;
 const Helpers = {
+	/**
+	 * Return seconds passed since Jan 1, 1970
+	 * @return {Number}
+	 */
 	unixSeconds: () => (Math.round(Date.now() / 1000)),
 
 	secondsToPrettyTime: () => {
 
 	},
 
-	// Convert a shorthand time format like `5m30s` to seconds 330
-	timeToSeconds: (time) => {
-		if (!time) {
-			throw new Error('timeToSeconds expects an argument!');
+	/**
+	 * Convert (a number) seconds to a time object with hours, minutes, total minutes and seconds.
+	 * Used by secondsToPrettyTime for to format the time display
+	 * @param  {Number}
+	 * @return {Object}
+	 */
+	secondsToTimeObj: (totalSeconds = 0) => {
+		let hours = 0;
+		let totalMinutes = Math.floor(totalSeconds / SIXTY);
+		let minutes = totalMinutes;
+		let seconds;
+
+		// Hours
+		if (totalSeconds >= THIRTYSIXHUNDRED) {
+			hours = Math.floor(totalSeconds / THIRTYSIXHUNDRED);
+		}
+
+		// Minutes
+		if (totalSeconds >= THIRTYSIXHUNDRED) {
+			minutes = Math.floor(totalSeconds % THIRTYSIXHUNDRED / SIXTY);
+		}
+		// Prepend 0 to minutes under TEN
+		if (minutes < TEN && hours > 0) {
+			minutes = '0' + minutes;
+		}
+		// Seconds
+		seconds = totalSeconds % SIXTY;
+		// Prepend 0 to seconds under TEN
+		if (seconds < TEN && (minutes > 0 || hours > 0)) {
+			seconds = '0' + seconds;
+		}
+
+		return {hours, minutes, totalMinutes, seconds, totalSeconds};
+	},
+
+	/**
+	 * Convert a shorthand time format like `5m30s` to seconds 330
+	 * @param  {String}
+	 * @return {Number}
+	 */
+	timeFormatToSeconds: (timeFormat) => {
+		if (!timeFormat) {
+			throw new Error('timeFormatToSeconds expects a string argument!');
 			return;
 		}
 
-		// Early return in case number is provided
-		if (!isNaN(Number(time))) {
-			return time;
+		// Early return in case a number is passed
+		if (!isNaN(Number(timeFormat))) {
+			return timeFormat;
 		}
 
-		time = time.toLowerCase();
+		timeFormat = timeFormat.toLowerCase();
+		let hrs = timeFormat.match(/\d{1,2}h/);		// Match 5h in 5h30m10s
+		let mins = timeFormat.match(/\d{1,2}m/);	// Match 30m in 5h30m10s
+		let secs = timeFormat.match(/\d{1,2}s/);	// Match 10s in 5h30m10s
 
-		let hrs = time.match(/\d{1,2}h/);
-		let mins = time.match(/\d{1,2}m/);
-		let secs = time.match(/\d{1,2}s/);
+		if (!hrs && !mins && !secs) {
+			throw new Error('Invalid string passed in timeFormatToSeconds!');
+			return;
+		}
 		let seconds = 0;
 
 		if (hrs) {
-			seconds += Number(hrs[0].replace('h', '') * 3600)
+			seconds += Number(hrs[0].replace('h', '') * THIRTYSIXHUNDRED)
 		}
 
 		if (mins) {
-			seconds += Number(mins[0].replace('m', '')) * 60;
+			seconds += Number(mins[0].replace('m', '')) * SIXTY;
 		}
 
 		if (secs) {
--- test/helpers-test.js
+++ test/helpers-test.js
@@ -6,15 +6,36 @@ test('test unixSeconds', (t) => {
 	t.end();
 });
 
-test('test timeToSeconds', (t) => {
-	t.equal(Helpers.timeToSeconds('5m30s'), 330);
-	t.equal(Helpers.timeToSeconds(330), 330);
-	t.equal(Helpers.timeToSeconds('30s'), 30);
-	t.equal(Helpers.timeToSeconds('5m'), 300);
-	t.equal(Helpers.timeToSeconds('5h'), 18000);
-	t.equal(Helpers.timeToSeconds('5h30m10s'), 19810);
+test('test timeFormatToSeconds', (t) => {
+	t.equal(Helpers.timeFormatToSeconds(330), 330);
+	t.equal(Helpers.timeFormatToSeconds('5m30s'), 330);
+	t.equal(Helpers.timeFormatToSeconds('30s'), 30);
+	t.equal(Helpers.timeFormatToSeconds('5m'), 300);
+	t.equal(Helpers.timeFormatToSeconds('5h'), 18000);
+	t.equal(Helpers.timeFormatToSeconds('5h30m10s'), 19810);
 	t.throws(function() {
-		Helpers.timeToSeconds();
+		Helpers.timeFormatToSeconds();
+	});
+	t.throws(function() {
+		Helpers.timeFormatToSeconds('invalid-format');
+	});
+	t.end();
+});
+
+test('test secondsToTimeObj', (t) => {
+	t.deepEqual(Helpers.secondsToTimeObj(100), {
+		hours: 0,
+		minutes: 1,
+		totalMinutes: 1,
+		seconds: 40,
+		totalSeconds: 100
+	});
+	t.deepEqual(Helpers.secondsToTimeObj(1000), {
+		hours: 0,
+		minutes: 16,
+		totalMinutes: 16,
+		seconds: 40,
+		totalSeconds: 1000
 	});
 	t.end();
 });
