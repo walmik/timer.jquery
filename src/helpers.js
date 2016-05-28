5548f1218907d4a414c65fc018d5515495c70946
Walmik <walmik.deshpande@gmail.com>
2016-05-27 20:41:41 -0700

    minor refactor

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,43 @@
 const Helpers = {
-	getUnixSeconds: () => (Math.round(Date.now() / 1000)),
+	unixSeconds: () => (Math.round(Date.now() / 1000)),
 
+	secondsToPrettyTime: () => {
+
+	},
+
+	// Convert a shorthand time format like `5m30s` to seconds 330
+	timeToSeconds: (time) => {
+		if (!time) {
+			throw new Error('timeToSeconds expects an argument!');
+			return;
+		}
+
+		// Early return in case number is provided
+		if (!isNaN(Number(time))) {
+			return time;
+		}
+
+		time = time.toLowerCase();
+
+		let hrs = time.match(/\d{1,2}h/);
+		let mins = time.match(/\d{1,2}m/);
+		let secs = time.match(/\d{1,2}s/);
+		let seconds = 0;
+
+		if (hrs) {
+			seconds += Number(hrs[0].replace('h', '') * 3600)
+		}
+
+		if (mins) {
+			seconds += Number(mins[0].replace('m', '')) * 60;
+		}
+
+		if (secs) {
+			seconds += Number(secs[0].replace('s', ''));
+		}
+
+		return seconds;
+	}
 	
 };
 
--- test/helpers-test.js
+++ test/helpers-test.js
@@ -1,7 +1,20 @@
 import test from 'tape';
 import Helpers from '../src/helpers.js';
 
-test('test helper functions', (t) => {
-	t.equal(Helpers.getUnixSeconds(), Math.round(Date.now() / 1000));
+test('test unixSeconds', (t) => {
+	t.equal(Helpers.unixSeconds(), Math.round(Date.now() / 1000));
+	t.end();
+});
+
+test('test timeToSeconds', (t) => {
+	t.equal(Helpers.timeToSeconds('5m30s'), 330);
+	t.equal(Helpers.timeToSeconds(330), 330);
+	t.equal(Helpers.timeToSeconds('30s'), 30);
+	t.equal(Helpers.timeToSeconds('5m'), 300);
+	t.equal(Helpers.timeToSeconds('5h'), 18000);
+	t.equal(Helpers.timeToSeconds('5h30m10s'), 19810);
+	t.throws(function() {
+		Helpers.timeToSeconds();
+	});
 	t.end();
 });
